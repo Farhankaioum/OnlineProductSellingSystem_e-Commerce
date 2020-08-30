af596ce8169491ee09820092f323ade2f1bc1073
FarhanKaioum <km1472233@gmail.com>
2020-08-31 00:27:57 +0600

    ApplicatinUser Added to inherit IdentityUser

--- Batch_3_Review-1/Startup.cs
+++ Batch_3_Review-1/Startup.cs
@@ -30,7 +30,7 @@ namespace Batch_3_Review_1
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();

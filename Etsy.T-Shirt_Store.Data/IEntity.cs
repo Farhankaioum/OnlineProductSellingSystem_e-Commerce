4e0b8b108c89c2308f3fe4711e6527e1900ed56c
FarhanKaioum <km1472233@gmail.com>
2020-08-31 11:05:45 +0600

    Data class library added with reposity, unitofwork, sqldataprovider

---
+++ Etsy.T-Shirt_Store.Data/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Etsy.T_Shirt_Store.Data
+{
+    public interface IEntity<T>
+    {
+        T Id { get; set; }
+    }
+}
---
+++ Etsy.T-Shirt_Store.Data/ISqlServerDataProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Etsy.T_Shirt_Store.Data
+{
+    public interface ISqlServerDataProvider<T>
+    {
+        (IList<T> result, int total, int totalDisplay) GetData(string procedureName, IList<(string key, object value, bool isOut)> parameters);
+    }
+}
